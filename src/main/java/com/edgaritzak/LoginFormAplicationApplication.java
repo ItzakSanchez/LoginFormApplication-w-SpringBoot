4184c3f093661f37020a18bfab2c3d9009c92283
Edgar Itzak Sanchez Rogers <109997241+ItzakSanchez@users.noreply.github.com>
2024-07-24 16:00:12 -0600

    Add files via upload

---
+++ src/main/java/com/edgaritzak/LoginFormAplicationApplication.java
@@ -0,0 +1,13 @@
+package com.edgaritzak.loginFormAplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoginFormAplicationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoginFormAplicationApplication.class, args);
+	}
+
+}
